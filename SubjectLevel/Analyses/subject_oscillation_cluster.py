20a7a9f08a8163d24227bacabef8c3f1dc659b27
jayfmil <jay.f.mil@gmail.com>
2017-07-28 13:54:53 -0700

    working on traveling waves. added a plot

--- SubjectLevel/Analyses/subject_oscillation_cluster.py
+++ SubjectLevel/Analyses/subject_oscillation_cluster.py
@@ -34,7 +34,7 @@ class SubjectElecCluster(SubjectAnalysis):
     """
 
     def __init__(self, task=None, subject=None, montage=0, use_json=True):
-        super(SubjectAnalysis, self).__init__(task=task, subject=subject, montage=montage, use_json=use_json)
+        super(SubjectElecCluster, self).__init__(task=task, subject=subject, montage=montage, use_json=use_json)
 
         self.task_phase_to_use = ['enc']  # ['enc'] or ['rec']
         self.recall_filter_func = ram_data_helpers.filter_events_to_recalled_norm
@@ -53,10 +53,20 @@ class SubjectElecCluster(SubjectAnalysis):
         # spatial distance considered near
         self.near_dist = 15.
 
+        # plus/minus this value when computer hilbert phase
+        self.hilbert_half_range = 1.5
+
         # number of electrodes needed to be considered a clust
         self.min_num_elecs = 5
 
+        # number of permutations to compute null r-square distribution
+        self.num_perms = 100
+
+        # dictionary will hold the cluter results
         self.clusters = {}
+
+        # should have an option to copmute the electrodes that comprise a traveling wave based on just the recalled
+        # (or not recalled) trials? Or look for both?
 
 
     def analysis(self):
@@ -93,45 +103,58 @@ class SubjectElecCluster(SubjectAnalysis):
         theta_r = np.stack([(x, y) for x in thetas for y in rs])
         params = np.stack([theta_r[:, 1] * np.cos(theta_r[:, 0]), theta_r[:, 1] * np.sin(theta_r[:, 0])], -1)
 
-        # for electrode in each cluster, compute phase at the mean cluster frequency
+        # for each frequency with clusters
         for freq in np.sort(self.clusters.keys()):
             self.clusters[freq]['cluster_wave_ang'] = []
             self.clusters[freq]['cluster_wave_freq'] = []
             self.clusters[freq]['cluster_r2_adj'] = []
+            self.clusters[freq]['mean_cluster_wave_ang'] = []
+            self.clusters[freq]['mean_cluster_wave_freq'] = []
+            self.clusters[freq]['mean_cluster_r2_adj'] = []
+
+            # for each cluster at this frequency
             for cluster_count, cluster_elecs in enumerate(self.clusters[freq]['elecs']):
                 cluster_freq = self.clusters[freq]['mean_freqs'][cluster_count]
 
+                # only compute for electrodess in this cluster
                 cluster_elecs_mono = np.unique(
                     list((itertools.chain(*self.subject_data['bipolar_pairs'][cluster_elecs].data))))
                 cluster_elecs_bipol = np.array(self.subject_data['bipolar_pairs'][cluster_elecs].data,
                                                dtype=[('ch0', '|S3'), ('ch1', '|S3')]).view(np.recarray)
 
-                # make plust minus range a parameter. make buffer a parameter
+                # load eeg and filter in the frequency band of interest
                 cluster_ts = self.load_eeg(self.subject_data.events.data.view(np.recarray), cluster_elecs_mono,
                                            cluster_elecs_bipol, self.start_time[0], self.end_time[0], 1.0,
-                                           pass_band=[cluster_freq-1.5, cluster_freq+1.5])
+                                           pass_band=[cluster_freq-self.hilbert_half_range, cluster_freq+self.hilbert_half_range])
 
+                # downsample to 250 Hz, compute phase, and replace the timeseries power data with phase data
                 cluster_ts = cluster_ts.resampled(250)
                 cluster_ts.data = np.angle(hilbert(cluster_ts, N=cluster_ts.shape[-1], axis=-1))
                 cluster_ts = cluster_ts.remove_buffer(1.0)
 
+                self.res['time_ax'] = cluster_ts['time'].data
+
+                # compute mean phase and phase difference between ref phase and each electrode phase
+                ref_phase = pycircstat.mean(cluster_ts.data, axis=0)
+                cluster_ts.data = pycircstat.cdiff(cluster_ts.data, ref_phase)
+
+                # compute PCA of 3d electrode coords to get 2d coords
                 xyz = np.stack(self.elec_xyz_indiv[cluster_elecs], 0)
                 xyz -= np.mean(xyz, axis=0)
                 pca = PCA(n_components=3)
                 norm_coords = pca.fit_transform(xyz)[:, :2]
 
-                # for each timepoint, for each event
-                # remove this loop too?????? Nope too much memory
-                # self.cluster_ts = cluster_ts
-                # self.norm_coords = norm_coords
-                # self.theta_r = theta_r
-                # self.params = params
-                # return
+                # compute mean cluster statistics
+                mean_rel_phase = pycircstat.mean(cluster_ts.data, axis=2)
+                mean_cluster_wave_ang, mean_cluster_wave_freq, mean_cluster_r2_adj = \
+                    self.circ_lin_regress(mean_rel_phase.T, norm_coords, theta_r, params)
+                self.clusters[freq]['mean_cluster_wave_ang'].append(mean_cluster_wave_ang)
+                self.clusters[freq]['mean_cluster_wave_freq'].append(mean_cluster_wave_freq)
+                self.clusters[freq]['mean_cluster_r2_adj'].append(mean_cluster_r2_adj)
 
                 cluster_wave_ang = np.empty(cluster_ts.T.shape[:2])
                 cluster_wave_freq = np.empty(cluster_ts.T.shape[:2])
                 cluster_r2_adj = np.empty(cluster_ts.T.shape[:2])
-
                 for t, cluster_this_time in enumerate(tqdm(cluster_ts.T)):
                     wave_ang, wave_freq, r2_adj = self.circ_lin_regress(cluster_this_time.data, norm_coords, theta_r,
                                                                         params)
@@ -142,10 +165,18 @@ class SubjectElecCluster(SubjectAnalysis):
                 self.clusters[freq]['cluster_wave_freq'].append(cluster_wave_freq)
                 self.clusters[freq]['cluster_r2_adj'].append(cluster_r2_adj)
 
-                    # for cluster_this_ev in cluster_this_time:
-                    #     wave_ang, wave_freq, r2_adj = self.circ_lin_regress(cluster_this_ev.data, norm_coords, theta_r, params)
-                # pdb.set_trace()
+                #### BIPOLAR DOES IT MATTER???????
 
+                # compute shuffled r-square values. Perform on the mean relative phase across time
+                # mean_rel_phase =  pycircstat.mean(cluster_ts.data, axis=2)
+                # norm_coords_shuf = norm_coords.copy()
+                # cluster_r2_adj_shufs = np.zeros(cluster_r2_adj.shape[-1])
+                # for shuf in range(self.num_perms):
+                #     np.random.shuffle(norm_coords_shuf)
+                # mean_cluster_wave_ang, mean_cluster_wave_freq, mean_r2_adj = self.circ_lin_regress(mean_rel_phase,
+                #                                                                                    norm_coords,
+                #                                                                                    theta_r, params)
+                #     pdb.set_trace()
 
 
 
@@ -236,6 +267,7 @@ class SubjectElecCluster(SubjectAnalysis):
 
         # compute adjusted r square
         r2_adj = 1 - ((1 - circ_corr_coef ** 2) * (n - 1)) / (n - 4)
+
         wave_ang = min_vals[:, 0]
         wave_freq = min_vals[:, 1]
 
@@ -288,7 +320,120 @@ class SubjectElecCluster(SubjectAnalysis):
 
         return dict((k, v) for k, v in all_clusters.items() if all_clusters[k]['elecs'])
 
-    # def
+    def plot_cluster_features_by_rec(self):
+
+        edges = np.arange(0, 2 * np.pi + .2, np.pi / 10)
+        angs = np.mean(np.stack([edges[1:], edges[:-1]]), axis=0)
+
+        red = '#8c564b'
+        blue = '#1f77b4'
+        recalled = self.recall_filter_func(self.task, self.subject_data.events.data, self.rec_thresh)
+
+        for k in self.clusters.keys():
+            for clust_num in range(len(self.clusters[k]['elecs'])):
+
+                #             fig, (ax1,ax2,ax3) = plt.subplots(1,3)
+                ax1 = plt.subplot2grid((2, 4), (0, 0), rowspan=2)
+                ax2 = plt.subplot2grid((2, 4), (0, 1), rowspan=2)
+                ax3 = plt.subplot2grid((2, 4), (0, 2), polar=True)
+                ax4 = plt.subplot2grid((2, 4), (1, 2), polar=True)
+                ax5 = plt.subplot2grid((2, 4), (0, 3), polar=True)
+                ax6 = plt.subplot2grid((2, 4), (1, 3), polar=True)
+                #             plt.gcf().subplots_adjust(wspace=0.9)
+
+                plt.suptitle('Freq: %.3f Hz, %d electrodes' % (
+                self.clusters[k]['mean_freqs'][clust_num], len(self.clusters[k]['elecs'][clust_num])),
+                             y=1.1)
+                plt.tight_layout()
+
+                # left panel, adjusted R^2
+                r2_rec = self.clusters[k]['mean_cluster_r2_adj'][clust_num][recalled]
+                r2_nrec = self.clusters[k]['mean_cluster_r2_adj'][clust_num][~recalled]
+                t, p = ttest_ind(r2_rec, r2_nrec, nan_policy='omit')
+                m = [np.nanmean(r2_rec), np.nanmean(r2_nrec)]
+                e = [sem(r2_rec, nan_policy='omit'), sem(r2_nrec, nan_policy='omit')]
+
+                ax1.bar([.2], m[0], .35, color=red, linewidth=3.5, yerr=e[0],
+                        error_kw=dict(elinewidth=3, ecolor='k', capsize=8, capthick=3))
+                ax1.bar([.8], m[1], .35, color=blue, linewidth=3.5, yerr=e[1],
+                        error_kw=dict(elinewidth=3, ecolor='k', capsize=8, capthick=3))
+                ax1.set_xticks([.2, .8])
+                ax1.set_xticklabels(['Rec', 'NRec'])
+                ax1.set_ylabel('Adjusted ${R^2}$')
+                ax1.set_title('p: %.3f' % p, fontdict={'fontsize': 14})
+                ax1.set_axisbelow(True)
+
+                # middle panel, wave direction
+                counts_rec = np.histogram(self.clusters[k]['mean_cluster_wave_ang'][clust_num][recalled], bins=edges)[0]
+                bars = ax3.bar(angs, counts_rec, width=np.pi / 10, bottom=0.0, zorder=10)
+                #             ax2.set_yticklabels('')
+                ax3.set_xticklabels('')
+                #             print(dir(ax2))
+                for r, bar in zip(counts_rec, bars):
+                    bar.set_edgecolor('k')
+                    bar.set_facecolor(red)
+                    bar.set_alpha(0.8)
+
+                counts_nrec = np.histogram(self.clusters[k]['mean_cluster_wave_ang'][clust_num][~recalled], bins=edges)[
+                    0]
+                bars = ax4.bar(angs, counts_nrec, width=np.pi / 10, bottom=0.0, zorder=10)
+                ax4.set_xticklabels('')
+                ax4.set_xlabel('Angle V1')
+                for r, bar in zip(counts_nrec, bars):
+                    bar.set_edgecolor('k')
+                    bar.set_facecolor(blue)
+                    bar.set_alpha(0.8)
+
+                pval, P = pycircstat.cmtest(self.clusters[k]['mean_cluster_wave_ang'][clust_num][recalled],
+                                            self.clusters[k]['mean_cluster_wave_ang'][clust_num][~recalled])
+                ax3.set_title('p: %.3f' % pval, fontdict={'fontsize': 14})
+
+                # middle panel, wave direction
+                counts_rec = \
+                np.histogram(pycircstat.mean(self.clusters[k]['cluster_wave_ang'][clust_num], axis=0)[recalled],
+                             bins=edges)[0]
+                bars = ax5.bar(angs, counts_rec, width=np.pi / 10, bottom=0.0, zorder=10)
+                #             ax2.set_yticklabels('')
+                ax5.set_xticklabels('')
+                for r, bar in zip(counts_rec, bars):
+                    bar.set_edgecolor('k')
+                    bar.set_facecolor(red)
+                    bar.set_alpha(0.8)
+
+                counts_nrec = \
+                np.histogram(pycircstat.mean(self.clusters[k]['cluster_wave_ang'][clust_num], axis=0)[~recalled],
+                             bins=edges)[0]
+                bars = ax6.bar(angs, counts_nrec, width=np.pi / 10, bottom=0.0, zorder=10)
+                ax6.set_xticklabels('')
+                ax6.set_xlabel('Angle V2')
+                for r, bar in zip(counts_nrec, bars):
+                    bar.set_edgecolor('k')
+                    bar.set_facecolor(blue)
+                    bar.set_alpha(0.8)
+
+                pval, P = pycircstat.cmtest(
+                    pycircstat.mean(self.clusters[k]['cluster_wave_ang'][clust_num], axis=0)[recalled],
+                    pycircstat.mean(self.clusters[k]['cluster_wave_ang'][clust_num], axis=0)[~recalled])
+                ax5.set_title('p: %.3f' % pval, fontdict={'fontsize': 14})
+
+                rvl_rec = pycircstat.resultant_vector_length(self.clusters[k]['cluster_wave_ang'][clust_num], axis=0)[
+                    recalled]
+                rvl_nrec = pycircstat.resultant_vector_length(self.clusters[k]['cluster_wave_ang'][clust_num], axis=0)[
+                    ~recalled]
+                t, p = ttest_ind(rvl_rec, rvl_nrec, nan_policy='omit')
+                m = [np.nanmean(rvl_rec), np.nanmean(rvl_nrec)]
+                e = [sem(rvl_rec, nan_policy='omit'), sem(rvl_nrec, nan_policy='omit')]
+
+                ax2.bar([.2], m[0], .35, color=red, linewidth=3.5, yerr=e[0],
+                        error_kw=dict(elinewidth=3, ecolor='k', capsize=8, capthick=3))
+                ax2.bar([.8], m[1], .35, color=blue, linewidth=3.5, yerr=e[1],
+                        error_kw=dict(elinewidth=3, ecolor='k', capsize=8, capthick=3))
+                ax2.set_xticks([.2, .8])
+                ax2.set_xticklabels(['Rec', 'NRec'])
+                ax2.set_ylabel('RVL')
+                ax2.set_title('p: %.3f' % p, fontdict={'fontsize': 14})
+                ax2.set_axisbelow(True)
+                plt.show()
 
     def normalize_spectra(self, X):
         """
