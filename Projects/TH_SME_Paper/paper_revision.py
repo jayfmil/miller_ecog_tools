0987339f6aac881216d24f921e2cd594871a0c3b
jayfmil <jay.f.mil@gmail.com>
2017-10-17 10:08:14 -0400

    working on paper revision

--- Projects/TH_SME_Paper/paper_revision.py
+++ Projects/TH_SME_Paper/paper_revision.py
@@ -46,7 +46,13 @@ def filter_out_bad_elecs(subj, bad_elec_table, onset_only=True, only_bad=False):
     subj.subject_data = subj.subject_data[:, :, ~bad_bools]
     return subj
 
-def plot_good_and_bad_hipp_elecs(subj):
+
+def plot_good_and_bad_hipp_elecs(subj,
+                                 table_path='/Users/jmiller/Documents/papers/jacobsPapers/TH_SME/bad_elecs.csv'):
+
+    bad_elec_table = pd.read_csv(table_path, index_col=0)
+
+    # load sme data
     sme = group_SME.GroupSME(load_data_if_file_exists=True, subject_settings='default_50_freqs',
                                 load_res_if_file_exists=False, bipolar=True, use_json=True,
                                 subjs=[subj],
@@ -54,6 +60,9 @@ def plot_good_and_bad_hipp_elecs(subj):
                                 do_not_compute=True,start_time=[0.0],end_time=[1.5],
                                 recall_filter_func=ram_data_helpers.filter_events_to_recalled_norm)
     sme.process()
+    subj_sme = sme.subject_objs[0]
+    subj_sme.load_data()
+    subj_sme = subject_exclusions.remove_abridged_sessions(subj_sme)
 
     # load move data
     move = group_move_vs_still.GroupMoveStill(subjs=[subj],
@@ -62,14 +71,31 @@ def plot_good_and_bad_hipp_elecs(subj):
                                               base_dir='/Users/jmiller/data/python',
                                               do_not_compute=True)
     move.process()
+    subj_move = move.subject_objs[0]
+    subj_move.load_data()
+    subj_move = subject_exclusions.remove_abridged_sessions(subj_move)
+
+    # filter to just good electrodes
+    subj_sme_good_elecs = filter_out_bad_elecs(subj_sme, bad_elec_table, onset_only=True, only_bad=False)
+    subj_move_good_elecs = filter_out_bad_elecs(subj_move, bad_elec_table, onset_only=True, only_bad=False)
 
     freqs_inds = (sme.subject_objs[0].freqs >= 1) & (sme.subject_objs[0].freqs <= 3)
-    left_sme = np.stack([np.nanmean(x.res['ts'][freqs_inds].mean(axis=0)[(x.elec_locs['Hipp']) & (~x.elec_locs['is_right'])]) for
-                     x in sme.subject_objs], axis=0)
-    right_sme = np.stack([np.nanmean(x.res['ts'][freqs_inds].mean(axis=0)[(x.elec_locs['Hipp']) & (x.elec_locs['is_right'])]) for
-                     x in sme.subject_objs], axis=0)
-    left_move = np.stack([np.nanmean(x.res['ts'][freqs_inds].mean(axis=0)[(x.elec_locs['Hipp']) & (~x.elec_locs['is_right'])]) for
-                     x in move.subject_objs], axis=0)
-    right_move = np.stack([np.nanmean(x.res['ts'][freqs_inds].mean(axis=0)[(x.elec_locs['Hipp']) & (x.elec_locs['is_right'])]) for
-                     x in move.subject_objs], axis=0)
+    left_hipp_good_elecs = (subj_sme_good_elecs.elec_locs['Hipp']) & (~subj_sme_good_elecs.elec_locs['is_right'])
+    right_hipp_good_elecs = (subj_sme_good_elecs.elec_locs['Hipp']) & (subj_sme_good_elecs.elec_locs['is_right'])
 
+    # left_sme = np.nanmean(subj_sme_good_elecs.res['ts'][freqs_inds].mean(axis=0)[left_hipp_elecs])
+    # right_sme = np.nanmean(subj_sme_good_elecs.res['ts'][freqs_inds].mean(axis=0)[right_hipp_elecs])
+    left_move_good_elecs = np.nanmean(subj_move_good_elecs.res['ts'][freqs_inds].mean(axis=0)[left_hipp_good_elecs])
+    right_move_good_elecs = np.nanmean(subj_move_good_elecs.res['ts'][freqs_inds].mean(axis=0)[right_hipp_good_elecs])
+
+    # now just bad electrodes
+    subj_sme_bad_elecs = filter_out_bad_elecs(subj_sme, bad_elec_table, onset_only=True, only_bad=True)
+    subj_move_bad_elecs = filter_out_bad_elecs(subj_move, bad_elec_table, onset_only=True, only_bad=True)
+
+    left_hipp_bad_elecs = (subj_sme_bad_elecs.elec_locs['Hipp']) & (~subj_sme_bad_elecs.elec_locs['is_right'])
+    right_hipp_bad_elecs = (subj_sme_bad_elecs.elec_locs['Hipp']) & (subj_sme_bad_elecs.elec_locs['is_right'])
+
+    # left_sme = np.nanmean(subj_sme_good_elecs.res['ts'][freqs_inds].mean(axis=0)[left_hipp_elecs])
+    # right_sme = np.nanmean(subj_sme_good_elecs.res['ts'][freqs_inds].mean(axis=0)[right_hipp_elecs])
+    left_move_bad_elecs = np.nanmean(subj_move_bad_elecs.res['ts'][freqs_inds].mean(axis=0)[left_hipp_bad_elecs])
+    right_move_bad_elecs = np.nanmean(subj_move_bad_elecs.res['ts'][freqs_inds].mean(axis=0)[right_hipp_bad_elecs])
