3ae26b3f88ec042459a9de3f201e82ed4433148b
jayfmil <jay.f.mil@gmail.com>
2018-08-29 14:15:27 -0700

    spike aligned BRI data

--- miller_ecog_tools/SubjectLevel/subject_BRI_data.py
+++ miller_ecog_tools/SubjectLevel/subject_BRI_data.py
@@ -12,7 +12,7 @@ class SubjectBRIData(SubjectDataBase):
 
     # Automatically set up the save directory path based on this design. See properties at the end of file. Any time
     # one of these attributes is modified, the save path will be automatically updated.
-    save_str_tmp = '{0}/{1}/{2:.3f}_{3:.3f}_ms/{4:.3f}_ds/{5}_qual/{6}/data'
+    save_str_tmp = '{0}/{1}/{2:d}_{3:d}_ms/{4:d}_ds/{5}_qual/{6}/data'
     attrs_in_save_str = ['base_dir', 'task', 'start_spike_ms', 'stop_spike_ms',
                          'downsample_rate', 'spike_qual_to_use', 'subject']
 
