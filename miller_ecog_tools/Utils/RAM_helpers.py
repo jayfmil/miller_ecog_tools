00cdbb879c48002d363dd7b46344e1af9476c282
jayfmil <jay.f.mil@gmail.com>
2018-09-21 13:50:57 -0400

    fixed monopolar check

--- miller_ecog_tools/Utils/RAM_helpers.py
+++ miller_ecog_tools/Utils/RAM_helpers.py
@@ -357,7 +357,7 @@ def load_eeg(events, rel_start_ms, rel_stop_ms, buf_ms=0, elec_scheme=None, nois
             eegfile = f'/protocols/r1/subjects/{events.iloc[0].subject}/experiments/{events.iloc[0].experiment}/sessions/{events.iloc[0].session}/ephys/current_processed/noreref/{eegfile}'
             with h5py.File(eegfile, 'r') as f:
                 if not np.array(f['monopolar_possible'])[0] == 1:
-                    print('Bipolar referencing not possible for {}'.format(events.iloc[0].subject))
+                    print('Monopolar referencing not possible for {}'.format(events.iloc[0].subject))
                     return
 
     # add buffer is using
