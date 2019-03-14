4b6a5d31b75d4e983f41f5a4a7390935f13d9054
jayfmil <jay.f.mil@gmail.com>
2019-03-14 12:37:18 -0400

    subject_bri_novelty now runs for all events, just events with activation, just items activation, and the inverse (items w/o activation, pairs w/o)

--- miller_ecog_tools/SubjectLevel/Analyses/subject_bri_novelty.py
+++ miller_ecog_tools/SubjectLevel/Analyses/subject_bri_novelty.py
@@ -219,14 +219,14 @@ class SubjectNoveltyAnalysis(SubjectAnalysisBase, SubjectBRIData):
 
                             # store the hilbert phases
                             spike_phase_hilbert_cond = spike_phases_helpbert[events_to_keep]
-                            novel_phases_hilbert = spike_phase_hilbert_cond[events.isFirst]
+                            novel_phases_hilbert = spike_phase_hilbert_cond[events[events_to_keep].isFirst]
                             novel_phases_hilbert = novel_phases_hilbert[np.array([len(x) > 0 for x in novel_phases])]
                             if novel_phases.shape[0] == 0:
                                 novel_phases_hilbert = []
                             else:
                                 novel_phases_hilbert = np.vstack(novel_phases_hilbert)
 
-                            rep_phases_hilbert = spike_phase_hilbert_cond[~events.isFirst]
+                            rep_phases_hilbert = spike_phase_hilbert_cond[~events[events_to_keep].isFirst]
                             rep_phases_hilbert = rep_phases_hilbert[np.array([len(x) > 0 for x in rep_phases])]
                             if rep_phases.shape[0] == 0:
                                 rep_phases_hilbert = []
