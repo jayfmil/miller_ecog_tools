2a39a43d9473e3f40ae848c130de43025c820587
jayfmil <jay.f.mil@gmail.com>
2019-04-16 10:12:37 -0400

    added force_analysis to subject_analysis to force the analysis to run even if the res file exists

--- miller_ecog_tools/SubjectLevel/Analyses/subject_bri_novelty_spike_phase_with_shuffle.py
+++ miller_ecog_tools/SubjectLevel/Analyses/subject_bri_novelty_spike_phase_with_shuffle.py
@@ -116,7 +116,8 @@ class SubjectBRINoveltySpikePhaseWithShuffleAnalysis(SubjectAnalysisBase, Subjec
 
                 # and channels
                 for channel_num, channel_grp in tqdm(session_grp.items()):
-                    res_channel_grp = res_file.create_group(channel_grp.name)
+                    if channel_grp.name not in res_file:
+                        res_channel_grp = res_file.create_group(channel_grp.name)
 
                     # load behavioral events
                     events = pd.read_hdf(self.subject_data.filename, channel_grp.name + '/event')
